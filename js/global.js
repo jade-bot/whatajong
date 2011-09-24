c00ca72a391c725c3914dd73a81c96fdd86f4856
Pau <masylum@gmail.com>
2011-09-24 12:36:06 -0700

    deck and shuffle

--- js/global.js
+++ js/global.js
@@ -6,14 +6,57 @@ var APP = {maps: {}};
 
   APP.event = new EventEmitter2();
 
-  // TODO: implement
   function _getDeck() {
-    return [];
+    var i, j, tiles = [];
+
+    for (i = 1; i <= 9; i++) {
+      //Bambu
+      for (j = 0; j < 4; j++) {
+        tiles[i + 9 * j] = "b" + i;
+      }
+      //Caracter
+      for (j = 0; j < 4; j++) {
+        tiles[36 + i + 9 * j] = "c" + i;
+      }
+      //Cercle
+      for (j = 0; j < 4; j++) {
+        tiles[72 + i + 9 * j] = "o" + i;
+      }
+    }
+
+    //Winds
+    for (i = 1; i <= 4; i++) {
+      tiles[108 + i] = "w1";
+      tiles[112 + i] = "w2";
+      tiles[116 + i] = "w3";
+      tiles[120 + i] = "w4";
+    }
+
+    //Honors
+    for (i = 1; i <= 8; i++) {
+      tiles[124 + i] = "h" + i;
+    }
+
+    //Dragons
+    for (i = 1; i <= 4; i++) {
+      tiles[132 + i] = "dc";
+      tiles[136 + i] = "df";
+      tiles[140 + i] = "dp";
+    }
+    return tiles;
   }
 
-  // TODO: implement
-  function _shuffle(_tiles) {
-    return _tiles;
+  function _shuffle(tiles) {
+    var i = tiles.length;
+    if (i === 0) {
+      return false;
+    }
+    while (i--) {
+      var j = Math.random() * ( i + 1 ) | 0;
+      tiles[i] = tiles[j];
+      tiles[j] = tiles[i];
+     }
+    return tiles;
   }
 
   // first time
