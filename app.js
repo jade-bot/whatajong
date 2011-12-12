990f7f447e6eb91fad47bff07698e46737c3e0bd
Pau <masylum@gmail.com>
2011-12-12 03:05:33 +0100

    better production settings

--- app.js
+++ app.js
@@ -175,5 +175,19 @@ everyauth.helpExpress(http);
 
 io = require('socket.io').listen(http);
 io.set('log level', 1);
+io.configure('production', function () {
+  io.enable('browser client minification'); // send minified client
+  io.enable('browser client etag');         // apply etag caching logic based on version number
+  io.enable('browser client gzip');         // gzip the file
+  io.set('log level', 1);                   // reduce logging
+  io.set('transports', [                    // enable all transports (optional if you want flashsocket)
+    'websocket'
+  , 'flashsocket'
+  , 'htmlfile'
+  , 'xhr-polling'
+  , 'jsonp-polling'
+  ]);
+});
+
 db = require('mongojs').connect(conf.mongodb.connection_url, ['users', 'rooms']);
 http.listen(conf.port || 3000);
